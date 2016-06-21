6e5e27e00abf988f9b7547602e0d9c52d7155b70
Wojtek <rybinski05@gmail.com>
2016-06-21 11:07:27 +0200

    test

---
+++ Ankieta.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: wtk
+ * Date: 21.06.2016
+ * Time: 10:58
+ */
+class Ankieta
+{
+    public static function doSomething()
+    {
+        var_dump('do something');
+    }
+}
